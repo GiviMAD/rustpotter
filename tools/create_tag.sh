99d9d8f5b7dfaa3eee133aca4fd50ebb99e362c7
Miguel Álvarez Díez <miguelwork92@gmail.com>
2022-05-09 15:54:54 +0200

    add create tag script

---
+++ tools/create_tag.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+VERSION=$(cat Cargo.toml | grep ^version | egrep -i -o '\d*\.\d*(\.\d*)?')
+TAG_NAME="v$VERSION"
+echo "creating $TAG_NAME"
+git tag -a $TAG_NAME -m "version $VERSION"
+git push origin $TAG_NAME
